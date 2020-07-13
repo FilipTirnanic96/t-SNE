86e1b78e0cc1f3e7738583d09008aa8ebbfdac06
Tirnanic <uic52421@contiwan.com>
2020-07-13 14:50:57 +0200

    Started simple t-SNE implementation

---
+++ Spyder/simple_TSNE.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 12:16:03 2020
+
+@author: uic52421
+"""
+import numpy as np
+
+
+def compute_pairwise_distances(X, metric, squared):
+    if (metric == "euclidean") :
+        distance = np.zeros((X.shape[0], X.shape[0]))
+        if(squared):
+            for i in np.arange(0, X.shape[0]):
+                 distance[i,:] = np.sum((X - X[i, :])**2, axis = 1)
+
+        else:
+            for i in np.arange(0, X.shape[0]):
+                 distance[i,:] = np.sqrt(np.sum((X - X[i, :])**2, axis = 1))
+                 
+        return distance
+        
+    else:
+        raise ValueError("Incorrect type of metric.")
+
+
+def TSNE(X, n_components = 2, perplexity = 30, n_iter = 1000, learning_rate = 200, momentum_alpha = 0):
+    # compute distances between training samples
+    distances = compute_pairwise_distances(X, "euclidean")   
+    
+    # compute pairwise affinities p(j|i) whith perplexity Perp
+    
+    # compute p(i,j) =  (p(j|i) +  p(i|j)) / (2n)
+    
+    # sample init y from Gauss ~ N(0, 10^(-4)*I)
+    y = np.random.multivariate_normal(np.zeros((X.shape[0],n_components)), 10**(-4) * np.identity(n_components), num_class_1)
+    
+    for i in np.arange(0, n_iter):
+        # compute q(i,j)
+        
+        # compute gradient(Cost func.)
+        
+        # update solution y(i) = y(i-1) + learning_rate * gradient(Cost func.)
+    
+        
+    return y
---
+++ Spyder/t-SNE_Sample_Code.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 11:00:20 2020
+
+@author: uic52421
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import time
+import simple_TSNE
+
+
+from sklearn.manifold import TSNE
+
+np.random.seed(50)
+num_class_1 = 1000
+num_class_2 = 1000
+
+# first class Gauss 2d
+mean1 = np.array([1,1])
+cov1 =  np.array([[1, 0.2],[0.2, 1]])
+X1 = np.random.multivariate_normal(mean1, cov1, num_class_1)
+y1 = np.zeros((num_class_1))
+# second class Gauss 2d
+mean2 = np.array([10,1])
+cov2 =  np.array([[0.1, 0],[0, 0.1]])
+X2 = np.random.multivariate_normal(mean2, cov2, num_class_2)
+y2 = np.ones((num_class_2))
+# visualise classes
+plt.plot(X1[:,0], X1[:,1], 'ro')
+plt.plot(X2[:,0], X2[:,1], 'bo')
+plt.xlim([-3, 13])
+plt.ylim([-4, 6])
+plt.legend(['class1', 'class2'])
+plt.title('Sytetic data')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.show()
+# execute t-SNE
+X = np.concatenate((X1, X2), axis = 0)
+y = np.concatenate((y1, y2), axis = 0)
+t_SNE = TSNE(n_components = 2, perplexity = 30)
+start_time = time.time()
+X_trans = t_SNE.fit_transform(X)
+elapsed_time = time.time() - start_time
+print("The execution of t-SNE last for ", elapsed_time, "s")
+# plot t-SNE
+plt.plot(X_trans[np.where(y == 0),0], X_trans[np.where(y == 0),1], 'ro')
+plt.plot(X_trans[np.where(y == 1),0], X_trans[np.where(y == 1),1], 'bo')
+plt.legend(['class1', 'class2'])
+plt.title('t-SNE on sytetic data')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.show()
